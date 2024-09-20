293ceb243aa2c0ec6632690fcf0a33e492127af8
hayatoyamaguchii <yamaguchihayatoo@gmail.com>
2024-09-20 19:58:15 +0900

    add masterブランチ作成

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import Test from "./components/test.tsx";
+
+function App() {
+    return (
+        <div>
+            <Test />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/test.tsx
@@ -0,0 +1,14 @@
+function Profile() {
+    return (
+        <img
+            src="https://i.imgur.com/QIrZWGIs.jpg"
+            alt="Alan L. Hart"
+        />
+    );
+}
+
+const Test = () => {
+    return <div>Hello, World!</div>;
+};
+
+export default Test;
