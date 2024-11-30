3aecefc42b124dfd2d0386eb9d649286e2885d86
hayatoyamaguchii <yamaguchihayatoo@gmail.com>
2024-12-01 01:53:27 +0900

    [feature]認証関連の実装。

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Route, Routes } from "react-router-dom";
 
-// ページのインポート
-import { default as Home } from "./pages/home";
-import { default as Profile } from "./pages/profile";
-import { default as Message } from "./pages/message";
-import { default as Record } from "./pages/record";
-import { default as Settings } from "./pages/settings";
-
-// BrowserRouterはApp.tsxで既に使っているため、main.tsxでは削除します
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />{" "}
-        {/* App.tsx内でBrowserRouterを使っているため、ここでは再度使う必要はありません */}
+        <App />
     </React.StrictMode>,
 );
